798b9fab8bb52b751927605602b3d49a87810469
gonzalejandro <aege21@gmail.com>
2018-04-21 13:01:32 -0300

    modified mountain border function and added temperature class

--- Tarea1/Mountain.py
+++ Tarea1/Mountain.py
@@ -1,11 +1,11 @@
 import numpy as np
-import tqdm
 import matplotlib.pyplot as plt
-
+import tqdm 
+from Temperature import Temperature
 
 class Mountain:
     def __init__(self, height, width, dh, digits):
-        self._digits = float(digits)/1000
+        self._d = float(digits)/1000
 
         self._height = height
         self._width = width
@@ -14,79 +14,93 @@ class Mountain:
         self._h = int(float(self._height)/self._dh)
         self._w = int(float(self._width)/self._dh)
 
-        self._matrix = np.ones((self._h, self._w))
-        self._mountain_border = []
-        self.__calculate_border()
+        self._matrix = np.zeros((self._h, self._w))
 
-    def __calculate_border(self):
-        dax = self.__scale_x(1200 + 400 * self._digits + 1)
-        for i in range(dax):
-            self._mountain_border.append(self._h)
+    def _get_border(self):
+        self._m_border = []
+        xa = 0
+        xb = 1200+400*self._d
+        for x in range(0, self.__scl_x(xb)):
+            self._m_border.append(self._h)
 
-        dbx = dax + self.__scale_x(120)
-        for i in range(dax, dbx):
-            self._mountain_border.append(self._h - 1)
+        xa = xb
+        xb = xa+120
+        for x in range(self.__scl_x(xb-xa)):
+            self._m_border.append(self.__inv_y(0))
 
-        dax = dbx
-        dbx += self.__scale_x(280)
-        last_peak = 0
-        for i in range(dax, dbx):
-            x = i - dax
-            h = int(x * (float(100)/300))
-            self._mountain_border.append(self.__inverse_y(h))
-            if i == dbx - 1:
-                last_peak = h
+        xa = xb
+        xb = xa+280
+        m = float(100)/300
+        for x in range(0, self.__scl_x(xb-xa)):
+            self._m_border.append(self.__inv_y(m*x))
 
-        dax = dbx
-        dbx += self.__scale_x(800)
-        for i in range(dax, dbx):
-            x = i - dax
-            h = int(x * (float(1500 + 200 * self._digits - last_peak)/800)) + last_peak
-            self._mountain_border.append(self.__inverse_y(h))
-            if i == dbx - 1:
-                last_peak = h
+        c = m*(xb-xa)
+        xa = xb
+        xb = xa+800
+        m = (1500+200*self._d-c)/800
+        for x in range(0, self.__scl_x(xb-xa)):
+            self._m_border.append(self.__inv_y(m*x+self.__scl_x(c)))
 
-        dax = dbx
-        dbx += self.__scale_x(300)
-        for i in range(dax, dbx):
-            x = i - dax
-            h = -int(x * (float(200)/300)) + last_peak
-            self._mountain_border.append(self.__inverse_y(h))
-            if i == dbx - 1:
-                last_peak = h
+        c = m*(xb-xa)+c
+        xa = xb
+        xb = xa+300
+        m = -float(200)/300
+        for x in range(0, self.__scl_x(xb-xa)):
+            self._m_border.append(self.__inv_y(m*x+self.__scl_x(c)))
 
-        dax = dbx
-        dbx += self.__scale_x(500)
-        for i in range(dax, dbx):
-            x = i - dax
-            h = int(x * (float(550 - 100 * self._digits)/500)) + last_peak
-            self._mountain_border.append(self.__inverse_y(h))
-            if i == dbx - 1:
-                last_peak = h
+        c = m*(xb-xa)+c
+        xa = xb
+        xb = xa+500
+        m = (550-100*self._d)/500
+        for x in range(0, self.__scl_x(xb-xa)):
+            self._m_border.append(self.__inv_y(m*x+self.__scl_x(c)))
 
-        dax = dbx
-        for i in range(dax, self._w):
-            x = i - dax
-            h = -int(x * (float(200)/300)) + last_peak
-            self._mountain_border.append(self.__inverse_y(h))
-            if i == dbx - 1:
-                last_peak = h
+        c = m*(xb-xa)+c
+        xa = xb
+        m = -m
+        for x in range(0, self._w-self.__scl_x(xa)):
+            self._m_border.append(self.__inv_y(m*x+self.__scl_x(c)))
 
-    def __scale_x(self, x):
-        return int(float(x * self._w)/4000)
+    def __scl_x(self, x):
+        return int(float(x)/self._dh)
 
-    def __scale_y(self, y):
-        return int(float(x * self._w)/2000)
+    def __scl_y(self, y):
+        return int(float(y)/self._dh)
 
-    def __inverse_y(self, y):
+    def __inv_y(self, y):
         return self._h - 1 - y
 
+
+
     def base_case(self, t):
+        self._temp = Temperature(t)
+        for y in range(self._h):
+            for x in range(self._w):
+                self._matrix[y][x] = self._temp.get_atm(self.__inv_y(y)*self._dh)
+        
+        self._get_border()
         for x in range(self._w):
             for y in range(self._h):
-                if self._mountain_border[x] <= y:
+                if y > self._m_border[x]:
                     self._matrix[y][x] = np.nan
-            
+
+        for y in range(self._h):
+            for x in range(self._w):
+                if y > self._m_border[x]:
+                    break
+                xy = np.isnan(self._matrix[y][x])
+                y_m1 = np.isnan(self._matrix[min(y+1, self._h-1)][x])
+                x_m1 = np.isnan(self._matrix[y][x-1])
+                x_p2 = np.isnan(self._matrix[y][min(self._w-1,x+1)])
+                if y <= self._m_border[x] and (xy or y_m1 or x_m1 or x_p2):
+                    h = self._dh*(self._h-1-y)
+                    self._matrix[y][x] = self._temp.get_cont(h)
+
+        for x in range(self._w):
+            if self._m_border[x] < self._h-1:
+                break
+            if self._m_border[x] == self._h-1:
+                self._matrix[self._h-1][x] = self._temp.get_plant()
 
     def plot(self):
         fig = plt.figure()
@@ -96,6 +110,7 @@ class Mountain:
         plt.show()
 
 
-m = Mountain(2000,  4000, 1, 991)
-m.base_case(10)
+
+m = Mountain(2000,  4000, 10, 991)
+m.base_case(0)
 m.plot()
---
+++ Tarea1/Temperature.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Temperature:
+	def __init__(self, t):
+		self._time = t
+
+	def get_cont(self, y):
+		if y >= 1800:
+			return 0
+		else:
+			return 20
+
+	def get_sea(self):
+		if self._time < 8:
+			return 4
+		if 8 <= self._time < 16:
+			return self._time*float(16)/8 + 4
+		else:
+			return -self._time*float(16)/8 + 20
+
+	def get_plant(self):
+		return 400*(2 + np.cos(self._time*np.pi/float(12)))
+
+	def get_atm(self, y):
+		return -y*float(6)/1000 + self.get_sea()

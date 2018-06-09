42efdf00bcd10e0b15785e88236e21c59cf7a6cb
gonzalejandro <aege21@gmail.com>
2018-06-09 12:23:34 -0400

    added destructive blocks opengl design

--- Tarea2/dblock.py
+++ Tarea2/dblock.py
@@ -21,11 +21,105 @@ class DBlock(Figure):
         super().__init__(self.pos, rgb)
 
     def figure(self):
+        lower = self.physics.scl_coord_res(self.rects[0].inf)
+        upper = self.physics.scl_coord_res(self.rects[0].sup)
+        upper += lower * -1
+        lower += lower * -1
+
         glBegin(GL_QUADS)
-        glColor3f(0 / 255, 255 / 255, 255 / 255)
-        cord = [(0, 0), (0, 50), (50, 50), (50, 0)]
-        for p in cord:
-            glVertex2f(p[0], p[1])
+
+        # Brick Background
+        glColor3f(50 / 255, 50 / 255, 50 / 255)
+        glVertex2f(lower.x, lower.y)
+        glVertex2f(lower.x, upper.y)
+        glVertex2f(upper.x, upper.y)
+        glVertex2f(upper.x, lower.y)
+
+        glEnd()
+
+        glBegin(GL_QUADS)
+
+        # Upper Brick
+        glColor3f(80 / 255, 80 / 255, 80 / 255)
+        glVertex2f(0.1 * upper.x, upper.y)
+        glVertex2f(0.1 * upper.x, 0.72 * upper.y)
+        glVertex2f(upper.x, 0.72 * upper.y)
+        glVertex2f(upper.x, upper.y)
+
+        # Mid left brick
+        glVertex2f(lower.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.39 * upper.y)
+        glVertex2f(lower.x, 0.39 * upper.y)
+
+        # Mid right brick
+        glVertex2f(0.4 * upper.x, 0.67 * upper.y)
+        glVertex2f(upper.x, 0.67 * upper.y)
+        glVertex2f(upper.x, 0.39 * upper.y)
+        glVertex2f(0.4 * upper.x, 0.39 * upper.y)
+
+        # Bot left brick
+        glVertex2f(lower.x, 0.34 * upper.y)
+        glVertex2f(lower.x, 0.06 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.06 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.34 * upper.y)
+
+        # Bot right brick
+        glVertex2f(0.7 * upper.x, 0.34 * upper.y)
+        glVertex2f(upper.x, 0.34 * upper.y)
+        glVertex2f(upper.x, 0.06 * upper.y)
+        glVertex2f(0.7 * upper.x, 0.06 * upper.y)
+
+        glEnd()
+
+        glBegin(GL_QUADS)
+        glColor3f(100 / 255, 100 / 255, 100 / 255)
+
+        # Top brick brightness
+        glVertex2f(0.1 * upper.x, upper.y)
+        glVertex2f(0.1 * upper.x, 0.95 * upper.y)
+        glVertex2f(upper.x, 0.95 * upper.y)
+        glVertex2f(upper.x, upper.y)
+
+        glVertex2f(0.95 * upper.x, upper.y)
+        glVertex2f(0.95 * upper.x, 0.72 * upper.y)
+        glVertex2f(upper.x, 0.72 * upper.y)
+        glVertex2f(upper.x, upper.y)
+
+        # Mid left brightness
+        glVertex2f(lower.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.62 * upper.y)
+        glVertex2f(lower.x, 0.62 * upper.y)
+
+        glVertex2f(0.25 * upper.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.67 * upper.y)
+        glVertex2f(0.3 * upper.x, 0.39 * upper.y)
+        glVertex2f(0.25 * upper.x, 0.39 * upper.y)
+
+        # Mid right brightness
+        glVertex2f(0.4 * upper.x, 0.67 * upper.y)
+        glVertex2f(upper.x, 0.67 * upper.y)
+        glVertex2f(upper.x, 0.62 * upper.y)
+        glVertex2f(0.4 * upper.x, 0.62 * upper.y)
+
+        # Bot left brightness
+        glVertex2f(lower.x, 0.34 * upper.y)
+        glVertex2f(lower.x, 0.29 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.29 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.34 * upper.y)
+
+        glVertex2f(0.55 * upper.x, 0.34 * upper.y)
+        glVertex2f(0.55 * upper.x, 0.06 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.06 * upper.y)
+        glVertex2f(0.6 * upper.x, 0.34 * upper.y)
+
+        # Bot right brightness
+        glVertex2f(0.7 * upper.x, 0.34 * upper.y)
+        glVertex2f(upper.x, 0.34 * upper.y)
+        glVertex2f(upper.x, 0.29 * upper.y)
+        glVertex2f(0.7 * upper.x, 0.29 * upper.y)
+
         glEnd()
 
     def is_destroyed(self):
